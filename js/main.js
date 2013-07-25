22afb58efa6f110086892f5b2d0b51a4faed8fda
N.S. Lin <twelendil@gmail.com>
2013-07-25 23:06:21 +0800

    #about style

--- js/main.js
+++ js/main.js
@@ -2313,6 +2313,14 @@
 
 
 
+(function() {
+  (function($) {
+    var bgc;
+    bgc = $('body').css('background-color');
+    return $('.hero-inner-post').css('background-color', bgc).css('border', "#ECECEC");
+  })(jQuery);
+
+}).call(this);
 // Load all Bootstrap JavaScripts
 
 
