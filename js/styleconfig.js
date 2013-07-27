b10a172367e810e87cf1b0f5052bd4873e8458f5
N.S. Lin <twelendil@gmail.com>
2013-07-27 13:29:25 +0800

    done #about content

--- js/styleconfig.js
+++ js/styleconfig.js
@@ -1,8 +1,4 @@
 (function() {
-  (function($) {
-    var bgc;
-    bgc = $('body').css('background-color');
-    return $('.hero-inner-post').css('background-color', bgc).css('border', "#ECECEC");
-  })(jQuery);
+
 
 }).call(this);
